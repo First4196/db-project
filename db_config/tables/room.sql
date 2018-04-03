80e86f7442ad302ad8ccf1c0ceabe0864ec56005
bT-53 <phanu.bjstp@gmail.com>
2018-04-03 21:17:08 +0700

    Create room.sql

---
+++ db_config/tables/room.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS room(
+    room_no INTEGER(5) NOT NULL,
+    building_id INTEGER(3) NOT NULL,
+    faculty_id CHAR(2) NOT NULL,
+    room_type VARCHAR(20),
+    seat_capacity INTEGER(4),
+    CONSTRAINT room_pk PRIMARY KEY (room_no,building_id,faculty_id),
+	  CONSTRAINT building_fk1 FOREIGN KEY (faculty_id) REFERENCES faculty(faculty_code),
+    CONSTRAINT building_fk2 FOREIGN KEY (building_id) REFERENCES building(building_id)
+);
