59285b31e3aada35d7d6982af13e5a4722581a32
bT-53 <phanu.bjstp@gmail.com>
2018-04-03 20:25:24 +0700

    Create faculty.sql

---
+++ db_config/tables/faculty.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS faculty(
+    faculty_code	CHAR(2)		NOT NULL,
+    name_en			  VARCHAR(50),
+    name_th			  VARCHAR(50),
+    name_abbrev		VARCHAR(15),
+    CONSTRAINT faculty_pk PRIMARY KEY (faculty_code)
+);
