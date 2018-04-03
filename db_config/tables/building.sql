188ba78ab794d251de7e7f2ee0f832fd9c2599b4
bT-53 <phanu.bjstp@gmail.com>
2018-04-03 20:50:53 +0700

    Update building.sql

--- db_config/tables/building.sql
+++ db_config/tables/building.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS building(
     building_id		INTEGER(3)	NOT NULL,
-    faculty			CHAR(2)		NOT NULL,
-    name_en			VARCHAR(50),
-    name_th			VARCHAR(50),
-    name_abbrev		VARCHAR(15),
+    faculty		CHAR(2)		NOT NULL,
+    name_en		VARCHAR(50),
+    name_th		VARCHAR(50),
+    name_abbrev	VARCHAR(15),
     latitude 		DECIMAL(10,8),
     longitude 		DECIMAL(11,8),
     CONSTRAINT building_pk PRIMARY KEY (building_id,faculty),
