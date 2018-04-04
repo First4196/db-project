b1916c8539e7ffed95a75ad451a9fc63429003ba
bT-53 <phanu.bjstp@gmail.com>
2018-04-04 09:36:50 +0700

    Update news.sql

--- db_config/tables/news.sql
+++ db_config/tables/news.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS news(
     course_semester INTEGER(1) NOT NULL,
     course_section INTEGER(2) NOT NULL,
     publish_time TIME NOT NULL,
-	title VARCHAR(20),
+    title VARCHAR(20),
     detail TEXT,
     CONSTRAINT news_pk PRIMARY KEY (course_id,course_year,course_semester,course_section,publish_time),
     CONSTRAINT news_fk FOREIGN KEY (course_id,course_year,course_semester,course_section) REFERENCES course_section(course_id,course_year,course_semester,course_section)
