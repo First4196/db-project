6e639f9d8b8a03742351ce129ec46fd79dedf8ac
bT-53 <phanu.bjstp@gmail.com>
2018-04-04 21:00:55 +0700

    Add faculty

--- db_config/tables/curriculum.sql
+++ db_config/tables/curriculum.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS curriculum(
     name_en VARCHAR(50),
     name_th VARCHAR(50),
     start_year INTEGER(4),
-    CONSTRAINT curricululm_pk PRIMARY KEY (curriculum_id)
+    faculty CHAR(2),
+    CONSTRAINT curricululm_pk PRIMARY KEY (curriculum_id),
+    CONSTRAINT cuuriculum_fk FOREIGN KEY (faculty) REFERENCES faculty(faculty_code)
 );
