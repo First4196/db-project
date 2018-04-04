f557a5b4ef50a3bd424259639f8bf7621cec591a
bT-53 <phanu.bjstp@gmail.com>
2018-04-04 21:07:16 +0700

    Add leader

--- db_config/tables/course_sem.sql
+++ db_config/tables/course_sem.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS course_sem(
     course_id VARCHAR(7) NOT NULL,
     course_year INTEGER(4) NOT NULL,
     course_semester INTEGER(1) NOT NULL,
+    leader VARCHAR(11) NOT NULL,
     CONSTRAINT course_sem_pk PRIMARY KEY (course_id, course_year, course_semester),
-    CONSTRAINT course_id_fk FOREIGN KEY (course_id) REFERENCES course(course_id)
+    CONSTRAINT course_id_fk1 FOREIGN KEY (course_id) REFERENCES course(course_id),
+    CONSTRAINT course_id_fk2 FOREIGN KEY (leader) REFERENCES professor(professor_id)
 );
