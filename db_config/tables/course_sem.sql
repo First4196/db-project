8b420321685b2a030f32b136963b723d0a0d3a5b
bT-53 <phanu.bjstp@gmail.com>
2018-04-04 21:35:51 +0700

    Add midterm and final

--- db_config/tables/course_sem.sql
+++ db_config/tables/course_sem.sql
@@ -3,7 +3,11 @@ CREATE TABLE IF NOT EXISTS course_sem(
     course_year INTEGER(4) NOT NULL,
     course_semester INTEGER(1) NOT NULL,
     leader VARCHAR(11) NOT NULL,
+    midterm_exam VARCHAR(50) NOT NULL,
+    final_exam VARCHAR(50) NOT NULL,
     CONSTRAINT course_sem_pk PRIMARY KEY (course_id, course_year, course_semester),
-    CONSTRAINT course_id_fk1 FOREIGN KEY (course_id) REFERENCES course(course_id),
-    CONSTRAINT course_id_fk2 FOREIGN KEY (leader) REFERENCES professor(professor_id)
+    CONSTRAINT course_sem_fk1 FOREIGN KEY (course_id) REFERENCES course(course_id),
+    CONSTRAINT course_sem_fk2 FOREIGN KEY (leader) REFERENCES professor(professor_id),
+    CONSTRAINT course_sem_fk3 FOREIGN KEY (midterm_exam) REFERENCES exam(exam_name),
+    CONSTRAINT course_sem_fk4 FOREIGN KEY (final_exam) REFERENCES exam(exam_name)
 );
