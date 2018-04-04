bb756351f56fd2f882762d7a3372fd35a8f065a8
bT-53 <phanu.bjstp@gmail.com>
2018-04-04 21:04:11 +0700

    Add faculty

--- db_config/tables/building.sql
+++ db_config/tables/building.sql
@@ -5,5 +5,7 @@ CREATE TABLE IF NOT EXISTS building(
     name_abbrev VARCHAR(15),
     latitude DECIMAL(10,8),
     longitude DECIMAL(11,8),
-    CONSTRAINT building_pk PRIMARY KEY (building_id)
+    faculty CHAR(2),
+    CONSTRAINT building_pk PRIMARY KEY (building_id),
+    CONSTRAINT building_fk FOREIGN KEY (faculty) REFERENCES faculty(faculty_code)
 );
