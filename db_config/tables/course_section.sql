e5181d3d00932847ba8df28213b7a99f1c28666c
bT-53 <phanu.bjstp@gmail.com>
2018-04-04 14:54:09 +0700

    Rename capacity
    Change section_capacity to capacity.

--- db_config/tables/course_section.sql
+++ db_config/tables/course_section.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS course_section(
     course_year INTEGER(4) NOT NULL,
     course_semester INTEGER(1) NOT NULL,
     course_section INTEGER(2) NOT NULL,
-    section_capacity INTEGER(4),
+    capacity INTEGER(4),
     student_count INTEGER(4),
     CONSTRAINT course_section_pk PRIMARY KEY (course_id, course_year, course_semester, course_section),
     CONSTRAINT course_section_fk1 FOREIGN KEY (course_id, course_year, course_semester) REFERENCES course_sem(course_id, course_year, course_semester)
