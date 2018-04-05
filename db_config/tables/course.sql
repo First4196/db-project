df540d2e092eaab640090ded646038600be89aa6
bT-53 <phanu.bjstp@gmail.com>
2018-04-05 15:09:04 +0700

    Update course.sql

--- db_config/tables/course.sql
+++ db_config/tables/course.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS course(
     course_name_th VARCHAR(50),
     course_abbrev VARCHAR(20),
     credit INTEGER(3),
-    prerequisite VARCHAR(7),
     CONSTRAINT course_pk PRIMARY KEY (course_id)
 );
