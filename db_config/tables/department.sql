708d55160680b27988e73bbb57c5f0377f068329
bT-53 <phanu.bjstp@gmail.com>
2018-04-05 13:45:32 +0700

    Update department.sql

--- db_config/tables/department.sql
+++ db_config/tables/department.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS department(
-    department_id INTEGER(4) NOT NULL,
+    department_id CHAR(4) NOT NULL,
     dname VARCHAR(50) NOT NULL,
     faculty CHAR(2),
     CONSTRAINT department_pk PRIMARY KEY (department_id),
