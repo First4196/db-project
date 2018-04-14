abc9befbfac68b74dd78f4050954b0aca1ff5b47
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 21:31:19 +0700

    Update department.sql

--- db_config/tables/department.sql
+++ db_config/tables/department.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS department(
     name_th VARCHAR(50),
     faculty CHAR(2),
     CONSTRAINT department_pk PRIMARY KEY (department_id),
-    CONSTRAINT department_fk FOREIGN KEY (faculty) REFERENCES faculty(faculty_code)
+    CONSTRAINT department_fk FOREIGN KEY (faculty) REFERENCES faculty(faculty_code),
+    CONSTRAINT department_name_not_null CHECK(name_en IS NOT NULL OR name_th IS NOT NULL)
 );
