d9f1423980373f324dbf7b1018ea3a0a11037112
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 18:57:01 +0700

    Update bill.sql

--- db_config/tables/bill.sql
+++ db_config/tables/bill.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS bill(
-	student_id VARCHAR(11) NOT NULL,
+    student_id VARCHAR(10) NOT NULL,
     academic_year INTEGER(4) NOT NULL,
     semester INTEGER(1) NOT NULL,
-	amount DECIMAL,
-	payment_status ENUM('Paid','Unpaid','Late1','Late2'),
+    amount DECIMAL,
+    payment_status ENUM('Paid','Unpaid','Late1','Late2'),
     CONSTRAINT bill_pk PRIMARY KEY (student_id,semester,academic_year),
     CONSTRAINT bill_fk FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
