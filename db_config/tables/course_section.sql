4f2a0afaba5be62893ef74f1ff8410a4273bbe4c
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 23:07:02 +0700

    Update course_section.sql

--- db_config/tables/course_section.sql
+++ db_config/tables/course_section.sql
@@ -7,5 +7,6 @@ CREATE TABLE IF NOT EXISTS course_section(
     student_count INTEGER(4),
     CONSTRAINT course_section_pk PRIMARY KEY (course_id, course_year, course_semester, course_section),
     CONSTRAINT course_section_fk1 FOREIGN KEY (course_id, course_year, course_semester)
-    REFERENCES course_sem(course_id, course_year, course_semester)
+    REFERENCES course_sem(course_id, course_year, course_semester),
+    CONSTRAINT student_non_neg_num CHECK(capacity >= 0 AND student_count >= 0)
 );
