20e23c27426454b7b4cd8176a0bc2c9b4fde50b0
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 21:02:38 +0700

    Update faculty.sql

--- db_config/tables/faculty.sql
+++ db_config/tables/faculty.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS faculty(
     name_en VARCHAR(50),
     name_th VARCHAR(50),
     name_abbrev VARCHAR(15),
-    CONSTRAINT faculty_pk PRIMARY KEY (faculty_code)
+    CONSTRAINT faculty_pk PRIMARY KEY (faculty_code),
+    CONSTRAINT faculty_name_not_null CHECK(name_en IS NOT NULL OR name_th IS NOT NULL)
 );
