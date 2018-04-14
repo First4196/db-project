9bb75e7fd305641283ef639fcc591bdf5f9e5773
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 19:05:03 +0700

    Update teaching.sql

--- db_config/tables/teaching.sql
+++ db_config/tables/teaching.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS teaching(
-	professor_id VARCHAR(30) NOT NULL,
+    professor_id VARCHAR(30) NOT NULL,
     course_id VARCHAR(7) NOT NULL,
     course_year INTEGER(4) NOT NULL,
     course_semester INTEGER(1) NOT NULL,
-	course_section INTEGER(2) NOT NULL,
+    course_section INTEGER(2) NOT NULL,
     CONSTRAINT teaching_pk PRIMARY KEY (professor_id,course_id,course_year,course_semester),
     CONSTRAINT teaching_fk1 FOREIGN KEY (professor_id) REFERENCES professor(professor_id),
     CONSTRAINT teaching_fk2 FOREIGN KEY (course_id,course_year,course_semester,course_section)
