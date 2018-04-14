1f0252d15a859fc047e39ad5d719df827c111521
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 18:37:06 +0700

    Update account.sql

--- db_config/tables/account.sql
+++ db_config/tables/account.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS account(
     username VARCHAR(30) NOT NULL,
     password VARCHAR(40) NOT NULL,
-    type ENUM('student','professor','staff'),
+    type ENUM('student','professor','staff') NOT NULL,
     CONSTRAINT account_pk PRIMARY KEY (username)
 );
