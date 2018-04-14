7b3ab74a950678816629c26d2c1fafaa1acde621
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 20:53:13 +0700

    Update course.sql

--- db_config/tables/course.sql
+++ db_config/tables/course.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS course(
     credit INTEGER(3),
     CONSTRAINT course_pk PRIMARY KEY (course_id),
     CONSTRAINT name_not_null CHECK(course_name_en IS NOT NULL
-		OR course_name_th IS NOT NULL),
-	CONSTRAINT non_negative_credit CHECK(credit >= 0)
+	OR course_name_th IS NOT NULL),
+    CONSTRAINT non_negative_credit CHECK(credit >= 0)
 );
