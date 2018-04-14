6446e29fe22852927b7c29dc30271394b11f7a94
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 19:40:39 +0700

    Update request.sql

--- db_config/tables/request.sql
+++ db_config/tables/request.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS request(
     student_id VARCHAR(10) NOT NULL,
     request_time DATETIME NOT NULL,
     form_name VARCHAR(30),
-    details VARCHAR(300),
+    details TEXT,
     status ENUM('Accepted','Rejected','Pending'),
     CONSTRAINT request_pk PRIMARY KEY (student_id,request_time),
     CONSTRAINT request_fk FOREIGN KEY (student_id) REFERENCES student(student_id)
