aae9803cbe579d2a8b7b26f641baa53996e1b0c7
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 22:36:13 +0700

    Update exam_arrangement.sql

--- db_config/tables/exam_arrangement.sql
+++ db_config/tables/exam_arrangement.sql
@@ -7,5 +7,6 @@ CREATE TABLE IF NOT EXISTS exam_arrangement(
 	finish_time TIME NOT NULL,
 	CONSTRAINT exam_pk PRIMARY KEY (room_no,building_id,exam_name,exam_date,start_time),
 	CONSTRAINT exam_fk1 FOREIGN KEY (exam_name) REFERENCES exam(exam_name),
-	CONSTRAINT exam_fk2 FOREIGN KEY (room_no,building_id) REFERENCES room(room_no,building_id)
+	CONSTRAINT exam_fk2 FOREIGN KEY (room_no,building_id) REFERENCES room(room_no,building_id),
+    	CONSTRAINT exam_time CHECK(start_time < finish_time)
 );
