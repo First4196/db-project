0b2d712a55f97008ee9e5b8eefd9be6fe7587ac1
bT-53 <phanu.bjstp@gmail.com>
2018-04-14 19:00:00 +0700

    Update record.sql

--- db_config/tables/record.sql
+++ db_config/tables/record.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS record(
-	student_id VARCHAR(11) NOT NULL,
+    student_id VARCHAR(11) NOT NULL,
     course_id VARCHAR(7) NOT NULL,
     course_year INTEGER(4) NOT NULL,
     course_semester INTEGER(1) NOT NULL,
-	grade ENUM('A','B+','B','C+','C','D+','D','F','W','S','U','X','I','M'),
+    grade ENUM('A','B+','B','C+','C','D+','D','F','W','S','U','X','I','M'),
     hidden BOOLEAN NOT NULL,
     CONSTRAINT record_pk PRIMARY KEY (student_id,course_id,course_year,course_semester),
     CONSTRAINT record_fk1 FOREIGN KEY (student_id) REFERENCES student(student_id),
